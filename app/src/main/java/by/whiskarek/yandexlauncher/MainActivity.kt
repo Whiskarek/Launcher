61cb4194d0526fabec898bfc19470c35c86b1ea4
Whiskarek <andrei5709978@gmail.com>
2019-02-02 22:44:49 +0300

    added click listener to github textview

--- app/src/main/java/by/whiskarek/yandexlauncher/MainActivity.kt
+++ app/src/main/java/by/whiskarek/yandexlauncher/MainActivity.kt
@@ -1,12 +1,20 @@
 package by.whiskarek.yandexlauncher
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.github).setOnClickListener {
+            val openGitHubPageIntent = Intent(Intent.ACTION_VIEW)
+            openGitHubPageIntent.data = Uri.parse("https://github.com/Whiskarek")
+            startActivity(openGitHubPageIntent)
+        }
     }
 }
