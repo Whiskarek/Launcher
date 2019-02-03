d7e8ce85ee0ba6843e69b6a09b92d5216b03676d
Whiskarek <andrei5709978@gmail.com>
2019-02-03 14:21:40 +0300

    configured deploying to fabric

--- app/src/main/java/by/whiskarek/yandexlauncher/MainActivity.kt
+++ app/src/main/java/by/whiskarek/yandexlauncher/MainActivity.kt
@@ -5,11 +5,14 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.github).setOnClickListener {
             val openGitHubPageIntent = Intent(Intent.ACTION_VIEW)
