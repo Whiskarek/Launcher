c0a0a18db663a2c1318b26c295f753d75a15caa0
Whiskarek <andrei5709978@gmail.com>
2019-02-04 01:42:55 +0300

    configured deploying on AppCenter

--- app/src/main/java/by/whiskarek/yandexlauncher/MainActivity.kt
+++ app/src/main/java/by/whiskarek/yandexlauncher/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.crashlytics.android.Crashlytics
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Fabric.with(this, Crashlytics())
+        AppCenter.start(application, "415c2e43-fe28-4d27-8abd-25be79341c57",
+                Analytics::class.java, Crashes::class.java)
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.github).setOnClickListener {
             val openGitHubPageIntent = Intent(Intent.ACTION_VIEW)
