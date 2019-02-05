18786f05cb9e5dcdf77eb7562604151db4369a38
Whiskarek <andrei5709978@gmail.com>
2019-02-05 21:53:28 +0300

    created custom update-window

--- app/src/main/java/by/whiskarek/yandexlauncher/MainActivity.kt
+++ app/src/main/java/by/whiskarek/yandexlauncher/MainActivity.kt
@@ -9,6 +9,7 @@ import com.crashlytics.android.Crashlytics
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
 import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +17,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Fabric.with(this, Crashlytics())
+        Distribute.setListener(AppCenterDistributeListener())
         AppCenter.start(application, BuildConfig.APPCENTER_KEY,
-                Analytics::class.java, Crashes::class.java)
+                Analytics::class.java, Crashes::class.java, Distribute::class.java)
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.github).setOnClickListener {
             val openGitHubPageIntent = Intent(Intent.ACTION_VIEW)
