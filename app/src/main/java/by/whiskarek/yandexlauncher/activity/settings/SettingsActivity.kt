5899fdfd44c865b40ea59bc91e13aac02498bc18
Whiskarek <andrei5709978@gmail.com>
2019-02-17 23:34:55 +0300

    [profile_activity] Configured settings activity

--- app/src/main/java/by/whiskarek/yandexlauncher/activity/settings/SettingsActivity.kt
+++ app/src/main/java/by/whiskarek/yandexlauncher/activity/settings/SettingsActivity.kt
@@ -3,12 +3,18 @@ package by.whiskarek.yandexlauncher.activity.settings
 import android.os.Bundle
 import by.whiskarek.yandexlauncher.R
 import android.view.MenuItem
-import by.whiskarek.yandexlauncher.BaseActivity
+import androidx.appcompat.widget.Toolbar
+import by.whiskarek.yandexlauncher.activity.BaseActivity
 
 class SettingsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settings_container, SettingsFragment())
