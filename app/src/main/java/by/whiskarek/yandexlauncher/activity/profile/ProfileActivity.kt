68f0a5e73abf09f2bb7b0f520afe134bf50d30e6
Whiskarek <andrei5709978@gmail.com>
2019-02-18 00:36:13 +0300

    [profile_activity] File ends with new line

--- app/src/main/java/by/whiskarek/yandexlauncher/activity/profile/ProfileActivity.kt
+++ app/src/main/java/by/whiskarek/yandexlauncher/activity/profile/ProfileActivity.kt
@@ -1,13 +1,33 @@
 package by.whiskarek.yandexlauncher.activity.profile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
 import by.whiskarek.yandexlauncher.R
+import by.whiskarek.yandexlauncher.activity.BaseActivity
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
