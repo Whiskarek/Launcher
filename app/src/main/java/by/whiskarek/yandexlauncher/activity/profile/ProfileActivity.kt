67c09a8f7fe5f746f91f0f2c63319dbadae197c3
Whiskarek <andrei5709978@gmail.com>
2019-02-17 23:34:15 +0300

    [profile_activity] Created Profile activity

---
+++ app/src/main/java/by/whiskarek/yandexlauncher/activity/profile/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package by.whiskarek.yandexlauncher.activity.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import by.whiskarek.yandexlauncher.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
