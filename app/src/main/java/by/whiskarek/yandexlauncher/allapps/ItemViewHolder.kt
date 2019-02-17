e6eacc3f1466d4568c33edb9c878f033ccc65660
Whiskarek <andrei5709978@gmail.com>
2019-02-17 16:23:54 +0300

    [launcher_activity] ViewHolder configured to be used with items which represents applications

--- app/src/main/java/by/whiskarek/yandexlauncher/allapps/ItemViewHolder.kt
+++ app/src/main/java/by/whiskarek/yandexlauncher/allapps/ItemViewHolder.kt
@@ -3,15 +3,21 @@ package by.whiskarek.yandexlauncher.allapps
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import by.whiskarek.yandexlauncher.AppInfo
 import by.whiskarek.yandexlauncher.R
-import by.whiskarek.yandexlauncher.views.SquareView
+import by.whiskarek.yandexlauncher.views.SquareImageView
+import kotlin.reflect.KProperty0
 
 class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val icon: SquareView = itemView.findViewById(R.id.item_icon)
+    private val icon: SquareImageView = itemView.findViewById(R.id.item_icon)
     private val name: TextView = itemView.findViewById(R.id.item_name)
 
-    fun bind(color: Int) {
-        icon.setBackgroundColor(color)
-        name.text = String.format("#%06X", (0xFFFFFF and color))
+    fun bind(app: AppInfo) {
+        icon.setImageDrawable(app.icon)
+        name.text = app.name
+    }
+
+    fun setOnClickListener(listener: View.OnClickListener) {
+        itemView.setOnClickListener(listener)
     }
 }
