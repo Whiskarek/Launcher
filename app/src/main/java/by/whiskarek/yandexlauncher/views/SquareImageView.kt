144f22216989bf3b3fb381ad0c5cf1cda57fe2fc
Whiskarek <andrei5709978@gmail.com>
2019-02-17 20:31:02 +0300

    [launcher_activity] Class extends AppCompatImageView

--- app/src/main/java/by/whiskarek/yandexlauncher/views/SquareImageView.kt
+++ app/src/main/java/by/whiskarek/yandexlauncher/views/SquareImageView.kt
@@ -2,14 +2,14 @@ package by.whiskarek.yandexlauncher.views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 import kotlin.math.min
 
 class SquareImageView @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
-) : ImageView(context, attrs, defStyleAttr) {
+) : AppCompatImageView(context, attrs, defStyleAttr) {
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val min = min(widthMeasureSpec, heightMeasureSpec)
         super.onMeasure(min, min)
