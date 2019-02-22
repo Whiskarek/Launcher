dc983eb15a8b5896a3bb0cee9e920f905b503a01
Whiskarek <andrei5709978@gmail.com>
2019-02-22 03:30:45 +0300

    [welcome_page_activity] Refactored BaseActivity

--- app/src/main/java/by/whiskarek/yandexlauncher/activity/BaseActivity.kt
+++ app/src/main/java/by/whiskarek/yandexlauncher/activity/BaseActivity.kt
@@ -5,55 +5,60 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import by.whiskarek.yandexlauncher.Constants
-import by.whiskarek.yandexlauncher.R
 import java.lang.IllegalArgumentException
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
+    private val childClassName = this.javaClass.asSubclass(this.javaClass).simpleName
     protected var showWelcomePage
         get() = BaseActivity.showWelcomePage
         private set(value) {
             BaseActivity.showWelcomePage = value
         }
+    var currentTheme
+        get() = BaseActivity.currentTheme
+        private set(value) {
+            BaseActivity.currentTheme = value
+        }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "onResume(). Before registering OnSharedPreferenceChangeListener")
+        Log.d(TAG, "$childClassName.onResume(). Before registering OnSharedPreferenceChangeListener")
         PreferenceManager
             .getDefaultSharedPreferences(this)
-            .registerOnSharedPreferenceChangeListener(this::onSharedPreferenceChanged)
-        Log.d(TAG, "onResume(). After registering OnSharedPreferenceChangeListener")
-        this.javaClass.asSubclass(this.javaClass)
+            .registerOnSharedPreferenceChangeListener(this)
+        Log.d(TAG, "$childClassName.onResume(). After registering OnSharedPreferenceChangeListener")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAG, "onPause(). Before unregistering OnSharedPreferenceChangeListener")
+        Log.d(TAG, "$childClassName.onPause(). Before unregistering OnSharedPreferenceChangeListener")
         PreferenceManager
             .getDefaultSharedPreferences(this)
-            .unregisterOnSharedPreferenceChangeListener(this::onSharedPreferenceChanged)
-        Log.d(TAG, "onPause(). After unregistering OnSharedPreferenceChangeListener")
+            .unregisterOnSharedPreferenceChangeListener(this)
+        Log.d(TAG, "$childClassName.onPause(). After unregistering OnSharedPreferenceChangeListener")
     }
 
     override fun setTheme(resid: Int) {
-        Log.d(TAG, "setTheme()")
-        if (currentTheme == -1) {
-            Log.d(TAG, "setTheme(). Loading settings from sharedPreferences")
+        Log.d(TAG, "$childClassName.setTheme()")
+        if (!loadedFromSharedPreferences) {
+            Log.d(TAG, "$childClassName.setTheme(). Loading settings from sharedPreferences")
             loadSettingsFromSharedPreferences()
-            Log.d(TAG, "setTheme(). Settings successfully loaded")
+            loadedFromSharedPreferences = true
+            Log.d(TAG, "$childClassName.setTheme(). Settings successfully loaded")
         }
         super.setTheme(currentTheme)
     }
 
-    private fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        Log.d(TAG, "onSharedPreferenceChanged()")
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        Log.d(TAG, "$childClassName.onSharedPreferenceChanged()")
         if (sharedPreferences == null)
             return
         when (key) {
-            Constants.PREFERENCE_KEY_THEME -> changeTheme(sharedPreferences, key)
-            Constants.PREFERENCE_KEY_MODEL -> changeModel(sharedPreferences, key)
-            Constants.PREFERENCE_KEY_SORT -> changeSort(sharedPreferences, key)
-            Constants.PREFERENCE_KEY_SHOW_WELCOME_PAGE_ON_NEXT_LOAD -> changeShowWelcomePage(sharedPreferences, key)
+            Constants.KEY_THEME -> changeTheme(sharedPreferences, key)
+            Constants.KEY_MODEL -> changeModel(sharedPreferences, key)
+            Constants.KEY_SORT -> changeSort(sharedPreferences, key)
+            Constants.KEY_SHOW_WELCOME_PAGE_ON_NEXT_LOAD -> changeShowWelcomePage(sharedPreferences, key)
         }
     }
 
@@ -79,58 +84,59 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
     protected open fun onThemeChanged(newTheme: Int) {
-        Log.d(TAG, "onThemeChanged(). Theme changed to $newTheme")
+        Log.d(TAG, "$childClassName.onThemeChanged(). Theme changed to $newTheme")
     }
 
     protected open fun onModelChanged(newModel: String) {
-        Log.d(TAG, "onThemeChanged(). Model changed to $newModel")
+        Log.d(TAG, "$childClassName.onModelChanged(). Model changed to $newModel")
     }
 
     protected open fun onSortChanged(newSort: String) {
-        Log.d(TAG, "onThemeChanged(). Sort changed to $newSort")
+        Log.d(TAG, "$childClassName.onSortChanged(). Sort changed to $newSort")
     }
 
     protected open fun onShowWelcomePageChanged(showWelcomePage: Boolean) {
-        Log.d(TAG, "onShowWelcomePageChanged(). ShowWelcomePage changed to $showWelcomePage")
+        Log.d(TAG, "$childClassName.onShowWelcomePageChanged(). ShowWelcomePage changed to $showWelcomePage")
     }
 
     private fun getTheme(themeType: String): Int = when (themeType) {
-        "light" -> R.style.AppThemeLight
-        "dark" -> R.style.AppThemeDark
-        else -> throw IllegalArgumentException("Unknown theme type")
+        Constants.THEME_LIGHT -> Constants.THEME_LIGHT_ID
+        Constants.THEME_DARK -> Constants.THEME_DARK_ID
+        else -> throw IllegalArgumentException("$childClassName.getTheme(). Unknown theme type")
     }
 
     private fun loadThemeFromSharedPreferences(sharedPreferences: SharedPreferences, key: String): Int {
-        val newThemeType = sharedPreferences.getString(key, "light")!!
+        val newThemeType = sharedPreferences.getString(key, Constants.THEME_LIGHT)!!
         return getTheme(newThemeType)
     }
 
     private fun loadModelFromSharedPreferences(sharedPreferences: SharedPreferences, key: String): String =
-        sharedPreferences.getString(key, "default")!!
+        sharedPreferences.getString(key, Constants.MODEL_DEFAULT)!!
 
     private fun loadSortFromSharedPreferences(sharedPreferences: SharedPreferences, key: String): String =
-        sharedPreferences.getString(key, "alphabet_high")!!
+        sharedPreferences.getString(key, Constants.SORT_DEFAULT)!!
 
     private fun loadShowWelcomePageFromSharedPreferences(sharedPreferences: SharedPreferences, key: String): Boolean =
         sharedPreferences.getBoolean(key, true)
 
     private fun loadSettingsFromSharedPreferences() {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        currentTheme = loadThemeFromSharedPreferences(sharedPreferences, Constants.PREFERENCE_KEY_THEME)
-        currentModel = loadModelFromSharedPreferences(sharedPreferences, Constants.PREFERENCE_KEY_MODEL)
-        currentSort = loadSortFromSharedPreferences(sharedPreferences, Constants.PREFERENCE_KEY_SORT)
+        currentTheme = loadThemeFromSharedPreferences(sharedPreferences, Constants.KEY_THEME)
+        currentModel = loadModelFromSharedPreferences(sharedPreferences, Constants.KEY_MODEL)
+        currentSort = loadSortFromSharedPreferences(sharedPreferences, Constants.KEY_SORT)
         showWelcomePage = loadShowWelcomePageFromSharedPreferences(
             sharedPreferences,
-            Constants.PREFERENCE_KEY_SHOW_WELCOME_PAGE_ON_NEXT_LOAD
+            Constants.KEY_SHOW_WELCOME_PAGE_ON_NEXT_LOAD
         )
     }
 
     companion object {
         private const val TAG = "BaseActivity"
 
-        private var currentTheme: Int = -1
-        private var currentModel: String = "default"
-        private var currentSort: String = "alphabet_high"
+        private var loadedFromSharedPreferences: Boolean = false
+        private var currentTheme: Int = Constants.THEME_DEFAULT_ID
+        private var currentModel: String = Constants.MODEL_DEFAULT
+        private var currentSort: String = Constants.SORT_DEFAULT
         private var showWelcomePage: Boolean = true
     }
 }
